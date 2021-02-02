99bf98cb687842c7795e417a2618ffbc05ffb576
wcgcyx <wcgcyx@gmail.com>
2021-02-02 14:42:24 +1000

    Integration of merkle trie & merkle proof.

--- internal/api/clientapi/standard_discover.go
+++ internal/api/clientapi/standard_discover.go
@@ -7,6 +7,7 @@ import (
 	"github.com/ConsenSys/fc-retrieval-gateway/internal/gateway"
 	"github.com/ConsenSys/fc-retrieval-gateway/internal/util"
 	"github.com/ConsenSys/fc-retrieval-gateway/pkg/fcrcrypto"
+	"github.com/ConsenSys/fc-retrieval-gateway/pkg/fcrmerkletrie"
 	"github.com/ConsenSys/fc-retrieval-gateway/pkg/fcrmessages"
 	"github.com/ConsenSys/fc-retrieval-gateway/pkg/logging"
 	"github.com/ant0ine/go-json-rest/rest"
@@ -34,8 +35,26 @@ func handleClientStandardCIDDiscover(w rest.ResponseWriter, request *fcrmessages
 	// Search for offesr.
 	offers, exists := g.Offers.GetOffers(pieceCID)
 
+	roots := make([]string, 0)
+	proofs := make([]fcrmerkletrie.FCRMerkleProof, 0)
+	fundedPaymentChannel := make([]bool, 0)
+
+	for _, offer := range offers {
+		trie := offer.GetMerkleTrie()
+		roots = append(roots, trie.GetMerkleRoot())
+		proof, err := trie.GenerateMerkleProof(pieceCID)
+		if err != nil {
+			s := "Internal error: Error generating proof."
+			logging.Error(s + err.Error())
+			rest.Error(w, s, http.StatusBadRequest)
+			return
+		}
+		proofs = append(proofs, *proof)
+		fundedPaymentChannel = append(fundedPaymentChannel, false) // TODO, Need to find a way to check if having payment channel set up for a given provider.
+	}
+
 	// Construct response
-	response, err := fcrmessages.EncodeClientStandardDiscoverResponse(pieceCID, nonce, exists, offers)
+	response, err := fcrmessages.EncodeClientStandardDiscoverResponse(pieceCID, nonce, exists, offers, roots, proofs, fundedPaymentChannel)
 	if err != nil {
 		s := "Internal error: Error encoding payload."
 		logging.Error(s + err.Error())

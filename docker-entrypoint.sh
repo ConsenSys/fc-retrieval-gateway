836f16c0d24c8ec53f1cb37a1c37599da8fcf796
ssaidali <samir.saidali@consensys.net>
2021-02-09 15:17:38 +0000

    feat: add docker-entrypoint.sh to Dockerfile

--- docker-entrypoint.sh
+++ docker-entrypoint.sh
@@ -2,7 +2,7 @@
 
 export CONTAINER_IP="$(awk 'END{print $1}' /etc/hosts)"
 
-echo "IP: $CONTAINER_IP"
+echo "Container IP: $CONTAINER_IP"
 echo "Starting service ..."
 
 ./main --host 0.0.0.0 --ip=$CONTAINER_IP

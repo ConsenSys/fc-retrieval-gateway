9abc187d64d453e236a705454a44daef1afb1e21
Peter  Robinson <drinkcoffee@eml.cc>
2021-02-16 14:33:47 +1000

    Rework dump code
    Signed-off-by: Peter  Robinson <drinkcoffee@eml.cc>

--- pkg/fcrmessages/dump.go
+++ pkg/fcrmessages/dump.go
@@ -15,25 +15,23 @@ import (
 
 // Dump the byte slice to a human-readable hex dump using the default
 // configuration.
-func dumpMessage(buf []byte) string { return defaultConfig.Dump(buf) }
-
-// Config allows customizing the dump configuration.
-type Config struct {
-	// Number of bytes from the input buffer to print in a single row. The default
-	// is 32.
-	Width int
+func dumpMessage(data []byte) string { 
+	var out bytes.Buffer
+	state := dumpState{32, 0, 0}
+	state.dump(&out, data)
+	return out.String()
 }
 
+
 type dumpState struct {
-	Config
+	width int
 	rowIndex    int
 	maxRowWidth int
 }
 
 func (s *dumpState) dump(out io.Writer, buf []byte) {
-	N := s.Width
-	for i := 0; i*N < len(buf); i++ {
-		a, b := i*N, (i+1)*N
+	for i := 0; i*s.width < len(buf); i++ {
+		a, b := i*s.width, (i+1)*s.width
 		if b > len(buf) {
 			b = len(buf)
 		}
@@ -46,85 +44,21 @@ func (s *dumpState) dump(out io.Writer, buf []byte) {
 		}
 		s.maxRowWidth = len(row)
 
-		fmt.Fprintf(out, "%5d: %s | %s\n", s.rowIndex*N, hex, ascii)
+		fmt.Fprintf(out, "%5d: %s | %s\n", s.rowIndex*s.width, hex, ascii)
 		s.rowIndex++
 	}
 }
 
-func (c Config) newDumpState() *dumpState {
-	s := &dumpState{Config: c}
-	if s.Width == 0 {
-		s.Width = kDefaultWidth
-	}
-	return s
-}
-
-// Dump converts the byte slice to a human-readable hex dump.
-func (c Config) Dump(data []byte) string {
-	var out bytes.Buffer
-	c.newDumpState().dump(&out, data)
-	return out.String()
-
-}
-
-// Read will read from the input io.Reader and write human-readable, formatted
-// hexdumps (with color annotations) to the output. The entire input reader is
-// consumed. Any errors other than io.EOF are returned.
-func (c Config) Stream(in io.Reader, out io.Writer) error {
-	s := c.newDumpState()
-	buf := make([]byte, 1*c.Width)
-	for {
-		n, err := io.ReadFull(in, buf)
-		s.dump(out, buf[:n])
-		if err == io.EOF || err == io.ErrUnexpectedEOF {
-			return nil
-		} else if err != nil {
-			return err
-		}
-	}
-}
-
-const kDefaultWidth = 32
-
-var defaultConfig = Config{kDefaultWidth}
-
-const (
-	kESC        = "\033["
-	kRESET      = "0"
-	kTERM       = "m"
-	kWHITE      = "37"
-	kGRAY       = "90"
-	kDARK_GREEN = "32"
-	kGREEN      = "92"
-	kDIM        = "2;"
-	kNO_ATTR    = ""
-)
-
-func color(str, attr, color string) string {
-	return kESC + attr + color + kTERM +
-		str +
-		kESC + kRESET + kTERM
-}
-
 func printable(data []byte) (hex, ascii string) {
 	s := string(data)
-	var hi, lo string
 	for i := 0; i < len(s); i++ {
-		if (i/8)%2 == 0 {
-			hi = kWHITE
-			lo = kGRAY
-		} else {
-			hi = kGREEN
-			lo = kDARK_GREEN
-		}
-
 		if s[i] < 32 || s[i] >= 127 {
-			ascii += color("░", kDIM, lo)
-			hex += color(fmt.Sprintf("%02x", s[i]), kDIM, hi)
+			ascii += "░"
 		} else {
-			ascii += color(string(s[i]), kNO_ATTR, hi)
-			hex += color(fmt.Sprintf("%02x", s[i]), kNO_ATTR, hi)
+			ascii += string(s[i])
 		}
+		hex += fmt.Sprintf("%02x ", s[i])
+
 		if i%4 == 3 {
 			hex += " "
 		}

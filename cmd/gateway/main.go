8d81946bff5b2fc1152d153e7c1f1b5dcb7c3c68
igor zenyuk <igor.zenyuk@gmail.com>
2021-04-08 17:10:51 +1000

    FSR-159 On New gateway or provider registering, trigger gateways and providers to update registered list

--- cmd/gateway/main.go
+++ cmd/gateway/main.go
@@ -3,12 +3,10 @@ package main
 // Copyright (C) 2020 ConsenSys Software Inc
 
 import (
-	"strings"
-	"time"
 	_ "github.com/joho/godotenv/autoload"
+	"strings"
 
 	"github.com/ConsenSys/fc-retrieval-common/pkg/logging"
-	"github.com/ConsenSys/fc-retrieval-register/pkg/register"
 	"github.com/ConsenSys/fc-retrieval-gateway/config"
 	"github.com/ConsenSys/fc-retrieval-gateway/internal/api/adminapi"
 	"github.com/ConsenSys/fc-retrieval-gateway/internal/api/clientapi"
@@ -16,7 +14,7 @@ import (
 	"github.com/ConsenSys/fc-retrieval-gateway/internal/api/providerapi"
 	"github.com/ConsenSys/fc-retrieval-gateway/internal/gateway"
 	"github.com/ConsenSys/fc-retrieval-gateway/internal/util"
-	"github.com/ConsenSys/fc-retrieval-gateway/internal/util/settings"
+	"github.com/ConsenSys/fc-retrieval-register/pkg/register"
 )
 
 func main() {
@@ -66,12 +64,6 @@ func main() {
 		return
 	}
 
-	// Get all registerd Gateways
-	go updateRegisteredGateways(appSettings, g)
-
-	// Get all registered Providers
-	go updateRegisteredProviders(appSettings, g)
-
 	// Configure what should be called if Control-C is hit.
 	util.SetUpCtrlCExit(gracefulExit)
 
@@ -79,127 +71,6 @@ func main() {
 
 	// Wait forever.
 	select {}
-}
-
-func updateRegisteredGateways(appSettings settings.AppSettings, g *gateway.Gateway) {
-	for {
-		logging.Debug("Update registered gateways")
-		gateways, err := register.GetRegisteredGateways(appSettings.RegisterAPIURL)
-		if err != nil {
-			logging.Error("Error in getting registered gateways: %s", err.Error())
-		} else {
-			// Check if nothing is changed.
-			update := false
-			g.RegisteredGatewaysMapLock.RLock()
-			if len(gateways) != len(g.RegisteredGatewaysMap) {
-				update = true
-			} else {
-				for _, gateway := range gateways {
-					// Skip itself
-					if gateway.NodeID == g.GatewayID.ToString() {
-						continue
-					}
-					storedInfo, exist := g.RegisteredGatewaysMap[strings.ToLower(gateway.NodeID)]
-					if !exist {
-						update = true
-						break
-					} else {
-						key, err := storedInfo.GetRootSigningKey()
-						rootSigningKey, err2 := key.EncodePublicKey()
-						key, err3 := storedInfo.GetSigningKey()
-						signingKey, err4 := key.EncodePublicKey()
-						if err != nil || err2 != nil || err3 != nil || err4 != nil {
-							logging.Error("Error in generating key string")
-							break
-						}
-						if gateway.Address != storedInfo.GetAddress() ||
-							gateway.NetworkInfoAdmin != storedInfo.GetNetworkInfoAdmin() ||
-							gateway.NetworkInfoClient != storedInfo.GetNetworkInfoClient() ||
-							gateway.NetworkInfoProvider != storedInfo.GetNetworkInfoProvider() ||
-							gateway.NetworkInfoGateway != storedInfo.GetNetworkInfoGateway() ||
-							gateway.RegionCode != storedInfo.GetRegionCode() ||
-							gateway.RootSigningKey != rootSigningKey ||
-							gateway.SigningKey != signingKey {
-							update = true
-							break
-						}
-					}
-				}
-			}
-			g.RegisteredGatewaysMapLock.RUnlock()
-			if update {
-				g.RegisteredGatewaysMapLock.Lock()
-				g.RegisteredGatewaysMap = make(map[string]register.RegisteredNode)
-				for _, gateway := range gateways {
-					// Skip itself
-					if gateway.NodeID == g.GatewayID.ToString() {
-						continue
-					}
-					logging.Info("Add to registered gateways map: nodeID=%+v", gateway.NodeID)
-					g.RegisteredGatewaysMap[strings.ToLower(gateway.NodeID)] = &gateway
-				}
-				g.RegisteredGatewaysMapLock.Unlock()
-			}
-		}
-		// Sleep for RegisterRefreshDuration duration, refresh every RegisterRefreshDuration duration
-		time.Sleep(appSettings.RegisterRefreshDuration)
-	}
-}
-
-func updateRegisteredProviders(appSettings settings.AppSettings, g *gateway.Gateway) {
-	for {
-		logging.Debug("Update registered providers")
-		providers, err := register.GetRegisteredProviders(appSettings.RegisterAPIURL)
-		if err != nil {
-			logging.Error("Error in getting registered providers: %s", err.Error())
-		} else {
-			// Check if nothing is changed.
-			update := false
-			g.RegisteredProvidersMapLock.RLock()
-			if len(providers) != len(g.RegisteredProvidersMap) {
-				update = true
-			} else {
-				for _, provider := range providers {
-					storedInfo, exist := g.RegisteredProvidersMap[strings.ToLower(provider.NodeID)]
-					if !exist {
-						update = true
-						break
-					} else {
-						key, err := storedInfo.GetRootSigningKey()
-						rootSigningKey, err2 := key.EncodePublicKey()
-						key, err3 := storedInfo.GetSigningKey()
-						signingKey, err4 := key.EncodePublicKey()
-						if err != nil || err2 != nil || err3 != nil || err4 != nil {
-							logging.Error("Error in generating key string")
-							break
-						}
-						if provider.Address != storedInfo.GetAddress() ||
-							provider.NetworkInfoAdmin != storedInfo.GetNetworkInfoAdmin() ||
-							provider.NetworkInfoClient != storedInfo.GetNetworkInfoClient() ||
-							provider.NetworkInfoGateway != storedInfo.GetNetworkInfoGateway() ||
-							provider.RegionCode != storedInfo.GetRegionCode() ||
-							provider.RootSigningKey != rootSigningKey ||
-							provider.SigningKey != signingKey {
-							update = true
-							break
-						}
-					}
-				}
-			}
-			g.RegisteredProvidersMapLock.RUnlock()
-			if update {
-				g.RegisteredProvidersMapLock.Lock()
-				g.RegisteredProvidersMap = make(map[string]register.RegisteredNode)
-				for _, provider := range providers {
-					logging.Info("Add to registered providers map: nodeID=%+v", provider.NodeID)
-					g.RegisteredProvidersMap[strings.ToLower(provider.NodeID)] = &provider
-				}
-				g.RegisteredProvidersMapLock.Unlock()
-			}
-		}
-		// Sleep for RegisterRefreshDuration duration, refresh every RegisterRefreshDuration duration
-		time.Sleep(appSettings.RegisterRefreshDuration)
-	}
 }
 
 func gracefulExit() {
---
+++ internal/api/adminapi/provider_administration.go
@@ -0,0 +1,124 @@
+package adminapi
+
+/*
+ * Copyright 2020 ConsenSys Software Inc.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License"); you may not use this file except in compliance with
+ * the License. You may obtain a copy of the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software distributed under the License is distributed on
+ * an "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the License for the
+ * specific language governing permissions and limitations under the License.
+ *
+ * SPDX-License-Identifier: Apache-2.0
+ */
+
+import (
+	"errors"
+	"github.com/ConsenSys/fc-retrieval-common/pkg/fcrmessages"
+	"github.com/ConsenSys/fc-retrieval-common/pkg/fcrtcpcomms"
+	"github.com/ConsenSys/fc-retrieval-common/pkg/logging"
+	"github.com/ConsenSys/fc-retrieval-gateway/internal/gateway"
+	"github.com/ConsenSys/fc-retrieval-gateway/internal/util/settings"
+	"github.com/ConsenSys/fc-retrieval-register/pkg/register"
+	"net"
+	"strings"
+)
+
+func handleAdminEnrollProvider(conn net.Conn, request *fcrmessages.FCRMessage, settings settings.AppSettings) error {
+	// Get core structure
+	g := gateway.GetSingleInstance()
+	if g.GatewayPrivateKey == nil {
+		return errors.New("this gateway hasn't been initialised by the admin")
+	}
+
+	nodeID, address, rootSigningKey, signingKey, regionCode, networkInfoGateway,
+		networkInfoClient, networkInfoAdmin, err :=
+		fcrmessages.DecodeGatewayAdminEnrollProviderRequest(request)
+	if err != nil {
+		return err
+	}
+
+	newProvider := register.ProviderRegister{
+		NodeID:             nodeID.ToString(),
+		Address:            address,
+		RootSigningKey:     rootSigningKey,
+		SigningKey:         signingKey,
+		RegionCode:         regionCode,
+		NetworkInfoGateway: networkInfoGateway,
+		NetworkInfoClient:  networkInfoClient,
+		NetworkInfoAdmin:   networkInfoAdmin,
+	}
+
+	ok := registerProvider(newProvider)
+	if !ok {
+		return errors.New("can not register a provider")
+	}
+
+	// Construct a message
+	response, err := fcrmessages.EncodeGatewayAdminEnrollProviderRequest(
+		nodeID,
+		address,
+		rootSigningKey,
+		signingKey,
+		regionCode,
+		networkInfoGateway,
+		networkInfoClient,
+		networkInfoAdmin)
+	if err != nil {
+		return err
+	}
+	// Sign message
+	if response.Sign(g.GatewayPrivateKey, g.GatewayPrivateKeyVersion) != nil {
+		return errors.New("error in signing message")
+	}
+	// Send message
+	return fcrtcpcomms.SendTCPMessage(conn, response, settings.TCPInactivityTimeout)
+}
+
+func registerProvider(newProvider register.ProviderRegister) (ok bool) {
+	logging.Debug("Registered a new provider")
+
+	// Get core structure
+	g := gateway.GetSingleInstance()
+	if g.GatewayPrivateKey == nil {
+		logging.Error("This gateway hasn't been initialised by the admin")
+		return false
+	}
+
+	update := false
+	g.RegisteredProvidersMapLock.RLock()
+	storedInfo, exist := g.RegisteredProvidersMap[strings.ToLower(newProvider.NodeID)]
+	if !exist {
+		update = true
+	} else {
+		key, err := storedInfo.GetRootSigningKey()
+		rootSigningKey, err2 := key.EncodePublicKey()
+		key, err3 := storedInfo.GetSigningKey()
+		signingKey, err4 := key.EncodePublicKey()
+		if err != nil || err2 != nil || err3 != nil || err4 != nil {
+			logging.Error("Error in generating key string")
+			return false
+		}
+		if newProvider.Address != storedInfo.GetAddress() ||
+			newProvider.NetworkInfoAdmin != storedInfo.GetNetworkInfoAdmin() ||
+			newProvider.NetworkInfoClient != storedInfo.GetNetworkInfoClient() ||
+			newProvider.NetworkInfoGateway != storedInfo.GetNetworkInfoGateway() ||
+			newProvider.RegionCode != storedInfo.GetRegionCode() ||
+			newProvider.RootSigningKey != rootSigningKey ||
+			newProvider.SigningKey != signingKey {
+
+			update = true
+		}
+	}
+	g.RegisteredProvidersMapLock.RUnlock()
+	if update {
+		logging.Info("Add to registered providers map: nodeID=%+v", newProvider.NodeID)
+		g.RegisteredProvidersMapLock.Lock()
+		g.RegisteredProvidersMap[strings.ToLower(newProvider.NodeID)] = &newProvider
+		g.RegisteredProvidersMapLock.Unlock()
+	}
+	return true
+}
